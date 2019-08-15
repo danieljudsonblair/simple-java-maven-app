509c2bd67d4e73a721c31425b4d4ee4e3af31522
s1191 <danieljudsonblair@gmail.com>
2019-08-15 14:34:17 -0400

    updated message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Try not to suffer \n alcohol dependency \n injection attack.";
 
     public App() {}
 
